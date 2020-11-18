9e539e5f36582eb6866b53828bae5195520809b3
shinyeon57 <noeys57@sookmyung.ac.kr>
2020-11-19 01:27:38 +0900

    -

---
+++ Untitled2.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#define N 6
+
+int	gameboard[N][N];
+void	init_stone();			//initial stone state
+void	init_othello();	
--- function.c
+++ function.c
@@ -1,49 +1,47 @@
+#include "Untitled2.h"
 #define N 6
-int init_othello(){
-	
-    extern int gameboard[N][N];
-	int	i;
-	int	k;
+#define	ROWS  6
+#define	COLS  6
+int gameboard[N][N];
 
-	printf("  0 1 2 3 4 5\n -------------\n");			//number of column
-	for(i=0;i<N;i++){									//for blank
-		//for first state of game_ ston's position
-		if(i==2){										
-			for(k=0;k<=N;k++){
-					if(k==0)
-						printf("%i|", i);				//number of column		
-					else if(k==3)
-						printf("0|");
-					else if(k==4)
-						printf("x|");
-					else
-						printf(" |");				
-							
-	    	}
-	}
-		else if(i==3){
-			for(k=0;k<=N;k++){
-					if(k==0)
-						printf("%i|", i);				//number of column		
-					else if(k==3)
-						printf("x|");
-					else if(k==4)
-						printf("0|");						
-					else
-						printf(" |");				
-							
-	    	}	
-		}
-		else{
-			for(k=0;k<=N;k++){
-					if(k==0)
-						printf("%i|", i);			//number of  column
-					else
-						printf(" |");				
-							
-	    	}	
-		}
-    printf("\n -------------\n");
-    }
-    	    
+void init_stone(/*int gameboard[][N]*/){
+		
+		int i,j;
+		
+		for(i=0;i<N;i++){
+			for(j=0;j<N;j++){
+				gameboard[i][j] = ' ';
+			}
+			}
+				
+		gameboard[N/2-1][N/2-1] = 'O';
+		gameboard[N/2-1][N/2] 	= 'X';
+		gameboard[N/2][N/2-1] 	= 'X';
+		gameboard[N/2][N/2] 	= 'O';
+}
+
+void init_othello(/*int gameboard[][N]*/){
+        
+		int i,j;
+
+	    printf("  0 1 2 3 4 5\n -------------\n");			//number of columns        
+        for(i=0;i<N;i++){
+             printf("%i|", i);								//number of rows
+            for(j=0;j<N;j++)
+            {  
+                printf("%c|", gameboard[i][j]);
+            }
+            printf("\n-------------\n");
+        }
+            
+}
+
+
+int	input__white_blank(){
+	
+	extern gameboard[N][N];
+	
+	printf("put a new white othello : %i", gameboard[N][N]);
+	scanf("%i", &gameboard[N][N]);
+	
 }
